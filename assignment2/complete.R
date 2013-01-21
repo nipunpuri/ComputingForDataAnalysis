f895b72d17778d070442d98c79ab3531e798dbc2
Vivek Hariharan <vivekhariharan11@gmail.com>
2013-01-21 14:19:29 -0600

    completed code for the three functions

---
+++ assignment2/complete.R
@@ -0,0 +1,28 @@
+complete <- function(directory, id = 1:332) {
+  ## 'directory' is a character vector of length 1 indicating
+  ## the location of the CSV files
+  
+  ## 'id' is an integer vector indicating the monitor ID numbers
+  ## to be used
+  
+  ## Return a data frame of the form:
+  ## id nobs
+  ## 1  117
+  ## 2  1041
+  ## ...
+  ## where 'id' is the monitor ID number and 'nobs' is the
+  ## number of complete cases
+  
+  countVector<-c()
+  for(index in 1:(length(id)))
+  {
+    formattedId <- formatC(id[index],width=3,flag="0")
+    fileName <- paste(directory,"/",formattedId,".csv",sep="")
+    data <- read.csv(fileName)
+    countOfObservations <- sum(complete.cases(data))
+    
+    countVector <- append(countVector,countOfObservations)
+  }
+  countDataFrame <-data.frame(id=id,nobs=countVector)
+  countDataFrame
+}
---
+++ assignment2/corr.R
@@ -0,0 +1,25 @@
+corr <- function(directory, threshold = 0) {
+  ## 'directory' is a character vector of length 1 indicating
+  ## the location of the CSV files
+  
+  ## 'threshold' is a numeric vector of length 1 indicating the
+  ## number of completely observed observations (on all
+  ## variables) required to compute the correlation between
+  ## nitrate and sulfate; the default is 0
+  
+  ## Return a numeric vector of correlations
+  corrVector<-integer(length=0)
+  for(index in 1:332)
+  {
+    formattedId <- formatC(index,width=3,flag="0")
+    filePath <- paste(directory,"/",formattedId,".csv",sep="")
+    data <- read.csv(filePath)
+    validData <-data[complete.cases(data),]
+       
+    if(length(validData[,1])>threshold)
+    {
+        corrVector <-append(corrVector,cor(validData[,2],validData[,3]))
+    }
+  }  
+  corrVector
+}
